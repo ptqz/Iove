321d6494020bfbc5778df0b09514d46759b69d6d
Mareks <m.silmanovics@gmail.com>
2023-01-13 11:15:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+document.getElementById("myH").style.color = "red";
+document.getElementById("myP").style.color = "red";
+document.getElementById("myH").style.fontFamily = "Verdana, Arial, sans-serif";
+document.getElementById("myP").style.fontFamily = "Verdana, Arial, sans-serif";
+
+
+
+function myFunction() {
+    var x = document.createElement("IMG");
+    document.getElementById("myH").innerHTML = "Даша";
+    document.getElementById("myP").innerHTML = "Я Тебя любою ";
+    x.setAttribute("src", "images/red-heart_2764-fe0f.png");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    x.setAttribute("alt", "The Pulpit Rock");
+    x.style.display = "block";
+    x.style.margin = "0 auto";
+    document.getElementById("knapp").style.display = "none";
+    document.body.appendChild(x);
+  }
